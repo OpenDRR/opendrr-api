b5d16def0d1aeb3422ec69f555c05a60c116f84c
Joost van Ulden <joost.vanulden@canada.ca>
2020-07-06 14:49:33 -0700

    Fixed path to .psql files

--- python/add_data.sh
+++ python/add_data.sh
@@ -10,7 +10,7 @@ done
 echo "\nImporting scenario outputs to PostGIS..."
 python DSRA_outputs2postgres_lfs.py --dsraModelDir https://github.com/OpenDRR/openquake-models/tree/master/deterministic/outputs --columnsINI DSRA_outputs2postgres.ini &&
 echo "\nGenerating indicators..."
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f /Create_scenario_risk_building_indicators_ALL.psql &&
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f /Create_scenario_risk_building_indicators_ALL_tables.psql &&
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f /Create_scenario_risk_sauid_indicators_ALL.psql &&
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f /Create_scenario_risk_sauid_indicators_ALL_tables.psql
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_scenario_risk_building_indicators_ALL.psql &&
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_scenario_risk_building_indicators_ALL_tables.psql &&
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_scenario_risk_sauid_indicators_ALL.psql &&
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_scenario_risk_sauid_indicators_ALL_tables.psql
