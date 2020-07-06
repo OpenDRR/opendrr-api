4578c769b14a9b53233e752798f5e7be4d4e33d3
Joost van Ulden <joost.vanulden@canada.ca>
2020-07-06 14:47:56 -0700

    Added wait for PostGres service ready state

--- python/add_data.sh
+++ python/add_data.sh
@@ -1,8 +1,15 @@
 set -e
 
-echo "Importing scenario outputs to PostGIS..."
+# make sure pg is ready to accept connections
+until pg_isready -h db-opendrr -p 5432 -U postgres
+do
+  echo "Waiting for postgres..."
+  sleep 2;
+done
+
+echo "\nImporting scenario outputs to PostGIS..."
 python DSRA_outputs2postgres_lfs.py --dsraModelDir https://github.com/OpenDRR/openquake-models/tree/master/deterministic/outputs --columnsINI DSRA_outputs2postgres.ini &&
-echo "Generating indicators..."
+echo "\nGenerating indicators..."
 psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f /Create_scenario_risk_building_indicators_ALL.psql &&
 psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f /Create_scenario_risk_building_indicators_ALL_tables.psql &&
 psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f /Create_scenario_risk_sauid_indicators_ALL.psql &&
