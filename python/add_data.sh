0bab9ea4fe11ab89c37d483c69d04ec425ee5b22
Ubuntu <ubuntu@ip-10-70-137-135.ca-central-1.compute.internal>
2020-11-04 20:03:08 +0000

    fixes to site level exposure handling

--- python/add_data.sh
+++ python/add_data.sh
@@ -73,7 +73,7 @@ done
 echo "\n Importing Physical Exposure Model into PostGIS"
 curl -H "Authorization: token ${GITHUB_TOKEN}" \
   -o BldgExp_CA_v2p5p3_master.csv \
-  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/exposure/general-building-stock/BldgExp_CA_v2p5p3_master.csv
+  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/exposure/general-building-stock/BldgExpRef_CA_master_v3.csv
 
 DOWNLOAD_URL=`grep -o '"download_url": *.*' BldgExp_CA_v2p5p3_master.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
 curl -o BldgExp_CA_v2p5p3_master.csv \
@@ -87,7 +87,7 @@ curl -H "Authorization: token ${GITHUB_TOKEN}" \
 DOWNLOAD_URL=`grep -o '"download_url": *.*' SiteExp_MetroVan_v4_master.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
 curl -o SiteExp_MetroVan_v4_master.csv \
   -L $DOWNLOAD_URL
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_canada_site_exposure_site.sql
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_canada_site_exposure_ste.sql
 
 echo "\n Importing VS30 Model into PostGIS..."
 curl -H "Authorization: token ${GITHUB_TOKEN}" \
@@ -110,7 +110,7 @@ python3 DSRA_sitemesh_gmf_xref.py  --eqScenario=$eqscenario
 done
 
 echo "\n Importing Rupture Model"
-python3 DSRA_ruptures2postgres.py --dsraRuptureDir="https://github.com/OpenDRR/openquake-models/tree/master/deterministic/ruptures" 
+python3 DSRA_ruptures2postgres.py --dsraRuptureDir="https://github.com/OpenDRR/scenario-catalogue/tree/master/deterministic/ruptures" 
 
 echo "\n Importing Census Data"
 curl -H "Authorization: token ${GITHUB_TOKEN}" \
@@ -182,7 +182,9 @@ psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_mh_thres
 echo "\n Generate Indicators"
 psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_physical_exposure_building_indicators_PhysicalExposure.sql
 psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_physical_exposure_sauid_indicators_view_PhysicalExposure.sql
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_physical_exposure_site_level_indicators_PhysicalExposure.sql
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_physical_exposure_building_indicators_PhysicalExposure_ste.sql
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_physical_exposure_sauid_indicators_view_PhysicalExposure_ste.sql
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_physical_exposure_site_level_indicators_PhysicalExposure_ste.sql
 psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_risk_dynamics_indicators.sql
 psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_social_vulnerability_sauid_indicators_SocialFabric.sql
 psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_MH_risk_sauid_ALL.sql
@@ -193,18 +195,22 @@ psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_MH_risk_sauid_
 echo "\n Generating indicator views..."
 for item in ${EQSCENARIO_LIST_LONGFORM[*]}
 do
-    SITE=$(echo $item | cut -f5- -d_)
+    SITE=$(echo $item | cut -f5- -d_ | cut -c 1-1)
     eqscenario=$(echo $item | cut -f-2 -d_)
-    echo $eqscenario
-done
-    if [ "$site" = "s" ] then
+    #echo $eqscenario
+    #echo $SITE
+    if [ "$SITE" = "s" ]
+    then
+        #echo "Site Model"
         python3 DSRA_createRiskProfileIndicators.py --eqScenario=$eqscenario --aggregation=site_level --exposureModel=site
         python3 DSRA_createRiskProfileIndicators.py --eqScenario=$eqscenario --aggregation=building --exposureModel=site
         python3 DSRA_createRiskProfileIndicators.py --eqScenario=$eqscenario --aggregation=sauid  --exposureModel=site
-    elif [ "$site" = "b"] then
+    elif [ "$SITE" = "b" ]
+    then
+        #echo "Building Model"
         python3 DSRA_createRiskProfileIndicators.py --eqScenario=$eqscenario --aggregation=building --exposureModel=building
         python3 DSRA_createRiskProfileIndicators.py --eqScenario=$eqscenario --aggregation=sauid  --exposureModel=building
-    
+    fi
 done 
 
 # make sure Elasticsearch is ready prior to creating indexes
@@ -214,8 +220,10 @@ until $(curl -sSf -XGET --insecure 'http://elasticsearch-opendrr:9200/_cluster/h
 done
 
 
-if [ "$loadDsraScenario" = true ] then
-    for eqscenario in ${EQSCENARIO_LIST[*]} do
+if [ "$loadDsraScenario" = true ]
+then
+    for eqscenario in ${EQSCENARIO_LIST[*]}
+    do
         echo "\nCreating elasticsearch indexes..."
         python3 dsra_postgres2es.py --eqScenario=$eqscenario --dbview="affected_people_casualties" --idField="building"
         python3 dsra_postgres2es.py --eqScenario=$eqscenario --dbview="building_damage_damage_state" --idField="building"
