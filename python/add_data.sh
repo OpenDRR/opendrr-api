31d77713ac82995e4b76a478258907fe163203b8
drotheram <drew.rotheram-clarke@canada.ca>
2021-02-05 10:49:16 -0800

    Reorder add_data.sh

--- python/add_data.sh
+++ python/add_data.sh
@@ -28,21 +28,6 @@ do
   sleep 2;
 done
 
-#get list of earthquake scenarios
-curl -H "Authorization: token ${GITHUB_TOKEN}" \
-  -O \
-  -L https://api.github.com/repos/OpenDRR/scenario-catalogue/contents/FINISHED
-EQSCENARIO_LIST=`grep -P -o '"name": "s_lossesbyasset_*.*r2' FINISHED | cut -f3- -d_`
-EQSCENARIO_LIST=($(echo $EQSCENARIO_LIST | tr ' ' '\n'))
-
-EQSCENARIO_LIST_LONGFORM=`grep -P -o '"name": "s_lossesbyasset_*.*r2.*csv' FINISHED | cut -f3- -d_`
-EQSCENARIO_LIST_LONGFORM=($(echo $EQSCENARIO_LIST_LONGFORM | tr ' ' '\n'))
-
-for item in ${!EQSCENARIO_LIST[@]} 
-do
-    EQSCENARIO_LIST[item]=${EQSCENARIO_LIST[item]:0:${#EQSCENARIO_LIST[item]}-3}
-done
-
 # get model-factory scripts
 git clone https://github.com/OpenDRR/model-factory.git --branch add-PSRA --depth 1 || (cd model-factory ; git pull)
 
@@ -52,6 +37,10 @@ git clone https://github.com/OpenDRR/boundaries.git --depth 1 || (cd boundaries 
 # copy model-factory scripts to working directory
 cp model-factory/scripts/*.* .
 rm -rf model-factory
+
+############################################################################################
+#######################     Process Exposure and Ancillary Data      #######################
+############################################################################################
 
 echo "\n Importing Census Boundaries"
 # create boundaries schema geometry tables from default geopackages.  Change ogr2ogr PATH / geopackage path if nessessary to run.
@@ -67,11 +56,130 @@ ogr2ogr -f "PostgreSQL" PG:"host=db-opendrr user=${POSTGRES_USER} dbname=${DB_NA
 rm -rf boundaries
 psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Update_boundaries_SAUID_table.sql
 
-echo "\n Importing scenario outputs into PostGIS..."
-for eqscenario in ${EQSCENARIO_LIST[*]}
-do
-python3 DSRA_outputs2postgres_lfs.py --dsraModelDir=$DSRA_REPOSITORY --columnsINI=DSRA_outputs2postgres.ini --eqScenario=$eqscenario
-done
+#Physical Exposure
+echo "\n Importing Physical Exposure Model into PostGIS"
+curl -H "Authorization: token ${GITHUB_TOKEN}" \
+  -o BldgExpRef_CA_master_v3p1.csv \
+  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/exposure/general-building-stock/BldgExpRef_CA_master_v3p1.csv
+
+DOWNLOAD_URL=`grep -o '"download_url": *.*' BldgExpRef_CA_master_v3p1.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
+curl -o BldgExpRef_CA_master_v3p1.csv \
+  -L $DOWNLOAD_URL
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_canada_exposure.sql
+
+curl -H "Authorization: token ${GITHUB_TOKEN}" \
+  -o PhysExpRef_MetroVan_v4.csv \
+  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/exposure/building-inventory/metro-vancouver/PhysExpRef_MetroVan_v4.csv
+
+DOWNLOAD_URL=`grep -o '"download_url": *.*' PhysExpRef_MetroVan_v4.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
+curl -o PhysExpRef_MetroVan_v4.csv \
+  -L $DOWNLOAD_URL
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_canada_site_exposure_ste.sql
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_site_exposure_to_building_and_sauid.sql
+
+#VS30
+echo "\n Importing VS30 Model into PostGIS..."
+curl -H "Authorization: token ${GITHUB_TOKEN}" \
+  -O \
+  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/earthquake/sites/regions/vs30_CAN_site_model_xref.csv
+DOWNLOAD_URL=`grep -o '"download_url": *.*' vs30_CAN_site_model_xref.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
+curl -o vs30_CAN_site_model_xref.csv \
+  -L $DOWNLOAD_URL
+
+curl -H "Authorization: token ${GITHUB_TOKEN}" \
+  -O \
+  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/earthquake/sites/regions/site-vgrid_CA.csv
+DOWNLOAD_URL=`grep -o '"download_url": *.*' site-vgrid_CA.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
+curl -o site-vgrid_CA.csv \
+  -L $DOWNLOAD_URL
+
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_vs_30_CAN_site_model.sql
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_vs_30_CAN_site_model_xref.sql
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_vs_30_BC_CAN_model_update_site_exposure.sql
+
+#Census Data
+echo "\n Importing Census Data"
+curl -H "Authorization: token ${GITHUB_TOKEN}" \
+  -o census-attributes-2016.csv \
+  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/exposure/census-ref-sauid/census-attributes-2016.csv?ref=ab1b2d58dcea80a960c079ad2aff337bc22487c5
+DOWNLOAD_URL=`grep -o '"download_url": *.*' census-attributes-2016.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
+curl -o census-attributes-2016.csv \
+  -L $DOWNLOAD_URL
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_2016_census_v3.sql
+
+
+echo "\n Importing Sovi"
+#need to source tables
+curl -H "Authorization: token ${GITHUB_TOKEN}" \
+  -O \
+  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/social-vulnerability/social-vulnerability-census.csv
+DOWNLOAD_URL=`grep -o '"download_url": *.*' social-vulnerability-census.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
+curl -o social-vulnerability-census.csv \
+  -L $DOWNLOAD_URL
+
+curl -H "Authorization: token ${GITHUB_TOKEN}" \
+  -O \
+  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/social-vulnerability/social-vulnerability-index.csv
+DOWNLOAD_URL=`grep -o '"download_url": *.*' social-vulnerability-index.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
+curl -o social-vulnerability-index.csv \
+  -L $DOWNLOAD_URL
+
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_sovi_index_canada_v2.sql
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_sovi_census_canada.sql
+
+echo "\n Importing LUTs"
+#Collapse Probability
+curl -H "Authorization: token ${GITHUB_TOKEN}" \
+  -o collapse_probability.csv \
+  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/exposure/general-building-stock/documentation/collapse_probability.csv?ref=73d15ca7e48291ee98d8a8dd7fb49ae30548f34e
+DOWNLOAD_URL=`grep -o '"download_url": *.*' collapse_probability.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
+curl -o collapse_probability.csv \
+  -L $DOWNLOAD_URL
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_collapse_probability_table.sql
+#Retrofit Costs
+curl -H "Authorization: token ${GITHUB_TOKEN}" \
+  -o retrofit_costs.csv \
+  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/exposure/general-building-stock/documentation/retrofit_costs.csv?ref=73d15ca7e48291ee98d8a8dd7fb49ae30548f34e
+
+DOWNLOAD_URL=`grep -o '"download_url": *.*' retrofit_costs.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
+curl -o retrofit_costs.csv \
+  -L $DOWNLOAD_URL
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_retrofit_costs_table.sql
+
+echo "\n Importing GHSL"
+curl -H "Authorization: token ${GITHUB_TOKEN}" \
+  -o mh-intensity-ghsl.csv \
+  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/natural-hazards/mh-intensity-ghsl.csv?ref=ab1b2d58dcea80a960c079ad2aff337bc22487c5
+DOWNLOAD_URL=`grep -o '"download_url": *.*' mh-intensity-ghsl.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
+curl -o mh-intensity-ghsl.csv \
+  -L $DOWNLOAD_URL
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_GHSL.sql
+
+echo "\n Importing MH Intensity"
+curl -H "Authorization: token ${GITHUB_TOKEN}" \
+  -o mh-intensity-sauid.csv \
+  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/natural-hazards/mh-intensity-sauid.csv?ref=ab1b2d58dcea80a960c079ad2aff337bc22487c5
+DOWNLOAD_URL=`grep -o '"download_url": *.*' mh-intensity-sauid.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
+curl -o mh-intensity-sauid.csv \
+  -L $DOWNLOAD_URL
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_mh_intensity_canada_v2.sql
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_mh_thresholds.sql
+
+echo "\n Generate Indicators"
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_physical_exposure_building_indicators_PhysicalExposure.sql
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_physical_exposure_sauid_indicators_view_PhysicalExposure.sql
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_physical_exposure_building_indicators_PhysicalExposure_ste.sql
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_physical_exposure_sauid_indicators_view_PhysicalExposure_ste.sql
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_physical_exposure_site_level_indicators_PhysicalExposure_ste.sql
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_risk_dynamics_indicators.sql
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_social_vulnerability_sauid_indicators_SocialFabric.sql
+psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_MH_risk_sauid_ALL.sql
+
+
+
+############################################################################################
+#######################     Process PSRA                             #######################
+############################################################################################
 
 echo "Importing Raw PSRA Tables"
 #Get list of provinces & territories
@@ -229,12 +337,6 @@ do
     sed -i '1d' $FILENAME
   done
 
-  # for PT in ${PT_LIST[@]}
-  # do
-  # echo $PT
-  # python3 PSRA_combineSrcLossTable.py --srcLossDir=/usr/src/app/ebRisk/${PT}
-  # done
-
   for file in ebR_*agg_curves-stats_b0.csv
   do
     sed -i '1d' $file
@@ -302,47 +404,30 @@ python3 PSRA_sqlWrapper.py --province=${PT} --sqlScript="psra_6.Create_psra_saui
 # python3 PSRA_sqlWrapper.py --province=${PT} --sqlScript="psra_8....."
 done
 
+############################################################################################
+#######################     Process DSRA                             #######################
+############################################################################################
 
-echo "\n Importing Physical Exposure Model into PostGIS"
-curl -H "Authorization: token ${GITHUB_TOKEN}" \
-  -o BldgExpRef_CA_master_v3p1.csv \
-  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/exposure/general-building-stock/BldgExpRef_CA_master_v3p1.csv
-
-DOWNLOAD_URL=`grep -o '"download_url": *.*' BldgExpRef_CA_master_v3p1.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
-curl -o BldgExpRef_CA_master_v3p1.csv \
-  -L $DOWNLOAD_URL
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_canada_exposure.sql
-
-curl -H "Authorization: token ${GITHUB_TOKEN}" \
-  -o PhysExpRef_MetroVan_v4.csv \
-  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/exposure/building-inventory/metro-vancouver/PhysExpRef_MetroVan_v4.csv
-
-DOWNLOAD_URL=`grep -o '"download_url": *.*' PhysExpRef_MetroVan_v4.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
-curl -o PhysExpRef_MetroVan_v4.csv \
-  -L $DOWNLOAD_URL
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_canada_site_exposure_ste.sql
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_site_exposure_to_building_and_sauid.sql
-
-
-echo "\n Importing VS30 Model into PostGIS..."
+#get list of earthquake scenarios
 curl -H "Authorization: token ${GITHUB_TOKEN}" \
   -O \
-  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/earthquake/sites/regions/vs30_CAN_site_model_xref.csv
-DOWNLOAD_URL=`grep -o '"download_url": *.*' vs30_CAN_site_model_xref.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
-curl -o vs30_CAN_site_model_xref.csv \
-  -L $DOWNLOAD_URL
+  -L ${DSRA_REPOSITORY}
+EQSCENARIO_LIST=`grep -P -o '"name": "s_lossesbyasset_*.*r2' FINISHED | cut -f3- -d_`
+EQSCENARIO_LIST=($(echo $EQSCENARIO_LIST | tr ' ' '\n'))
 
-curl -H "Authorization: token ${GITHUB_TOKEN}" \
-  -O \
-  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/earthquake/sites/regions/site-vgrid_CA.csv
-DOWNLOAD_URL=`grep -o '"download_url": *.*' site-vgrid_CA.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
-curl -o site-vgrid_CA.csv \
-  -L $DOWNLOAD_URL
+EQSCENARIO_LIST_LONGFORM=`grep -P -o '"name": "s_lossesbyasset_*.*r2.*csv' FINISHED | cut -f3- -d_`
+EQSCENARIO_LIST_LONGFORM=($(echo $EQSCENARIO_LIST_LONGFORM | tr ' ' '\n'))
 
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_vs_30_CAN_site_model.sql
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_vs_30_CAN_site_model_xref.sql
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_vs_30_BC_CAN_model_update_site_exposure.sql
+for item in ${!EQSCENARIO_LIST[@]} 
+do
+    EQSCENARIO_LIST[item]=${EQSCENARIO_LIST[item]:0:${#EQSCENARIO_LIST[item]}-3}
+done
 
+echo "\n Importing scenario outputs into PostGIS..."
+for eqscenario in ${EQSCENARIO_LIST[*]}
+do
+python3 DSRA_outputs2postgres_lfs.py --dsraModelDir=$DSRA_REPOSITORY --columnsINI=DSRA_outputs2postgres.ini --eqScenario=$eqscenario
+done
 
 echo "Importing Shakemap"
 #Make a list of Shakemaps in the repo and download the raw csv files
@@ -387,86 +472,8 @@ do
     echo " " 
 done 
 
-
 echo "\n Importing Rupture Model"
 python3 DSRA_ruptures2postgres.py --dsraRuptureDir="https://github.com/OpenDRR/scenario-catalogue/tree/master/deterministic/ruptures" 
-
-echo "\n Importing Census Data"
-curl -H "Authorization: token ${GITHUB_TOKEN}" \
-  -o census-attributes-2016.csv \
-  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/exposure/census-ref-sauid/census-attributes-2016.csv?ref=ab1b2d58dcea80a960c079ad2aff337bc22487c5
-DOWNLOAD_URL=`grep -o '"download_url": *.*' census-attributes-2016.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
-curl -o census-attributes-2016.csv \
-  -L $DOWNLOAD_URL
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_2016_census_v3.sql
-
-
-echo "\n Importing Sovi"
-#need to source tables
-curl -H "Authorization: token ${GITHUB_TOKEN}" \
-  -O \
-  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/social-vulnerability/social-vulnerability-census.csv
-DOWNLOAD_URL=`grep -o '"download_url": *.*' social-vulnerability-census.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
-curl -o social-vulnerability-census.csv \
-  -L $DOWNLOAD_URL
-
-curl -H "Authorization: token ${GITHUB_TOKEN}" \
-  -O \
-  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/social-vulnerability/social-vulnerability-index.csv
-DOWNLOAD_URL=`grep -o '"download_url": *.*' social-vulnerability-index.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
-curl -o social-vulnerability-index.csv \
-  -L $DOWNLOAD_URL
-
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_sovi_index_canada_v2.sql
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_sovi_census_canada.sql
-
-echo "\n Importing LUTs"
-#Collapse Probability
-curl -H "Authorization: token ${GITHUB_TOKEN}" \
-  -o collapse_probability.csv \
-  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/exposure/general-building-stock/documentation/collapse_probability.csv?ref=73d15ca7e48291ee98d8a8dd7fb49ae30548f34e
-DOWNLOAD_URL=`grep -o '"download_url": *.*' collapse_probability.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
-curl -o collapse_probability.csv \
-  -L $DOWNLOAD_URL
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_collapse_probability_table.sql
-#Retrofit Costs
-curl -H "Authorization: token ${GITHUB_TOKEN}" \
-  -o retrofit_costs.csv \
-  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/exposure/general-building-stock/documentation/retrofit_costs.csv?ref=73d15ca7e48291ee98d8a8dd7fb49ae30548f34e
-
-DOWNLOAD_URL=`grep -o '"download_url": *.*' retrofit_costs.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
-curl -o retrofit_costs.csv \
-  -L $DOWNLOAD_URL
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_retrofit_costs_table.sql
-
-echo "\n Importing GHSL"
-curl -H "Authorization: token ${GITHUB_TOKEN}" \
-  -o mh-intensity-ghsl.csv \
-  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/natural-hazards/mh-intensity-ghsl.csv?ref=ab1b2d58dcea80a960c079ad2aff337bc22487c5
-DOWNLOAD_URL=`grep -o '"download_url": *.*' mh-intensity-ghsl.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
-curl -o mh-intensity-ghsl.csv \
-  -L $DOWNLOAD_URL
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_GHSL.sql
-
-echo "\n Importing MH Intensity"
-curl -H "Authorization: token ${GITHUB_TOKEN}" \
-  -o mh-intensity-sauid.csv \
-  -L https://api.github.com/repos/OpenDRR/model-inputs/contents/natural-hazards/mh-intensity-sauid.csv?ref=ab1b2d58dcea80a960c079ad2aff337bc22487c5
-DOWNLOAD_URL=`grep -o '"download_url": *.*' mh-intensity-sauid.csv | cut -f2- -d: | tr -d '"'| tr -d ',' `
-curl -o mh-intensity-sauid.csv \
-  -L $DOWNLOAD_URL
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_mh_intensity_canada_v2.sql
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_table_mh_thresholds.sql
-
-echo "\n Generate Indicators"
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_physical_exposure_building_indicators_PhysicalExposure.sql
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_physical_exposure_sauid_indicators_view_PhysicalExposure.sql
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_physical_exposure_building_indicators_PhysicalExposure_ste.sql
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_physical_exposure_sauid_indicators_view_PhysicalExposure_ste.sql
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_physical_exposure_site_level_indicators_PhysicalExposure_ste.sql
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_risk_dynamics_indicators.sql
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_social_vulnerability_sauid_indicators_SocialFabric.sql
-psql -h db-opendrr -U ${POSTGRES_USER} -d ${DB_NAME} -a -f Create_MH_risk_sauid_ALL.sql
 
 echo "\n Generating indicator views..."
 for item in ${EQSCENARIO_LIST_LONGFORM[*]}
@@ -488,6 +495,10 @@ do
         python3 DSRA_createRiskProfileIndicators.py --eqScenario=$eqscenario --aggregation=sauid  --exposureModel=building
     fi
 done 
+
+############################################################################################
+#######################     Import Data from PostGIS to ElasticSearch   ####################
+############################################################################################
 
 # make sure Elasticsearch is ready prior to creating indexes
 until $(curl -sSf -XGET --insecure 'http://elasticsearch-opendrr:9200/_cluster/health?wait_for_status=yellow' > /dev/null); do
