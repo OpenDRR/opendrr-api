23eecbc2590457806e31cb0164b4b7cc3d36ed78
drotheram <drew.rotheram-clarke@canada.ca>
2021-04-12 11:48:15 -0700

    use model-factory branch in development

--- python/add_data.sh
+++ python/add_data.sh
@@ -35,7 +35,7 @@ do
 done
 
 # get model-factory scripts
-git clone https://github.com/OpenDRR/model-factory.git --depth 1 || (cd model-factory ; git pull)
+git clone https://github.com/OpenDRR/model-factory.git --branch update_sovi_hazthreat_feb2021 --depth 1 || (cd model-factory ; git pull)
 
 # get boundary files
 git clone https://github.com/OpenDRR/boundaries.git --depth 1 || (cd boundaries ; git pull)
