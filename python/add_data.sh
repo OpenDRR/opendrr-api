8f6467e022cc94ca1417c1406a6678b3676cf7c7
Anthony Fok <anthony.fok@canada.ca>
2021-06-02 09:01:29 -0600

    add_data: Convert more echos and comments to LOG() etc.

--- python/add_data.sh
+++ python/add_data.sh
@@ -77,7 +77,7 @@ set_synchronous_commit() {
   fi
   local on_off="$1"
 
-  echo "  - psql: Setting synchronous_commit TO $on_off..."
+  LOG "psql: Setting synchronous_commit TO $on_off..."
   psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$DB_NAME" -a \
     -c "SHOW synchronous_commit;"
   psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$DB_NAME" -a \
@@ -100,7 +100,7 @@ run_ogr2ogr() {
   local src_datasource_name="boundaries/$id.gpkg"
   local nln="boundaries.$(basename $id)"
 
-  echo "  - ogr2ogr: Importing $src_datasource_name into $DB_NAME..."
+  LOG "ogr2ogr: Importing $src_datasource_name into $DB_NAME..."
 
   ogr2ogr -t_srs "$srs_def" \
 	  -f PostgreSQL \
@@ -118,7 +118,7 @@ run_psql() {
   fi
   local input_file="$1"
 
-  echo "  - psql: Running $input_file..."
+  LOG "psql: Running $input_file..."
   psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$DB_NAME" -a -f "$input_file"
 }
 
@@ -139,7 +139,7 @@ fetch_csv_lfs() {
 
   mkdir -p github-api/$(dirname $path)
 
-  echo $repo/$path
+  INFO "$repo/$path"
   curl -s -o "$response" \
     --retry 999 --retry-max-time 0 \
     -H "Authorization: token ${GITHUB_TOKEN}" \
@@ -148,10 +148,11 @@ fetch_csv_lfs() {
   local download_url=$(jq -r '.download_url' "$response")
   local size=$(jq -r '.size' "$response")
 
+  # TODO: Actually use these values for verification
   echo download_url=$download_url
   echo size=$size
 
-  echo "${FUNCNAME[0]}: Download from $download_url"
+  LOG "Download from $download_url"
   curl -o "$output_file" -L "$download_url" --retry 999 --retry-max-time 0
 }
 
@@ -167,7 +168,7 @@ fetch_csv_xz() {
   local output_file=$(basename $path | sed -e 's/?.*//')
   local response
   local path_dir=$(dirname "$path")
-  echo $path_dir
+  INFO $path_dir
 
   # Fetch directory listing
   mkdir -p github-api/$path_dir
@@ -179,7 +180,7 @@ fetch_csv_xz() {
     -L "https://api.github.com/repos/$owner/$repo-xz/contents/$path_dir"
 
   local download_url=$(jq -r '.[] | select(.name == "'"$output_file"'.xz") | .download_url' $response)
-  echo "${FUNCNAME[0]}: Download from $download_url"
+  LOG "${FUNCNAME[0]}: Download from $download_url"
   curl -o "$output_file.xz" -L "$download_url" --retry 999 --retry-max-time 0
 
   # TODO: Keep the compressed file somewhere, uncompress when needed
@@ -335,9 +336,11 @@ set_synchronous_commit off
 #######################     Process Exposure and Ancillary Data      #######################
 ############################################################################################
 
-echo -e "\n Importing Census Boundaries"
+LOG "# Process Exposure and Ancillary Data"
 
-echo " - Trying to download pre-generated PostGIS database dump (for speed)..."
+LOG "## Importing Census Boundaries"
+
+INFO "Trying to download pre-generated PostGIS database dump (for speed)..."
 if RUN curl -O -v --retry 999 --retry-max-time 0 https://opendrr.eccp.ca/file/OpenDRR/opendrr-boundaries.dump || \
    RUN curl -O -v --retry 999 --retry-max-time 0 https://f000.backblazeb2.com/file/OpenDRR/opendrr-boundaries.dump
 then
@@ -346,9 +349,7 @@ then
     | while IFS= read -r line; do printf '%s %s\n' "$(date "+%Y-%m-%d %H:%M:%S")" "$line"; done
 else
   WARN "Unable to fetch opendrr-boundaries.dump."
-  WARN "Fallback to fetching CSV via Git LFS:"
-
-  # Get boundary files
+  WARN "Fallback to fetching boundaries CSV files via Git LFS:"
   RUN git clone https://github.com/OpenDRR/boundaries.git --depth 1 || (cd boundaries ; RUN git pull)
 
   # Create boundaries schema geometry tables from default geopackages.
@@ -367,7 +368,8 @@ RUN run_psql Update_boundaries_SAUID_table.sql
 
 
 # Physical Exposure
-echo -e "\n Importing Physical Exposure Model into PostGIS"
+LOG "## Importing Physical Exposure Model into PostGIS"
+
 RUN fetch_csv model-inputs \
   exposure/general-building-stock/BldgExpRef_CA_master_v3p1.csv
 RUN run_psql Create_table_canada_exposure.sql
@@ -377,7 +379,8 @@ RUN fetch_csv model-inputs \
 RUN run_psql Create_table_canada_site_exposure_ste.sql
 
 # VS30
-echo -e "\n Importing VS30 Model into PostGIS..."
+LOG "## Importing VS30 Model into PostGIS"
+
 RUN fetch_csv model-inputs \
   earthquake/sites/regions/vs30_CAN_site_model_xref.csv
 
@@ -388,13 +391,14 @@ RUN run_psql Create_table_vs_30_CAN_site_model.sql
 RUN run_psql Create_table_vs_30_CAN_site_model_xref.sql
 
 # Census Data
-echo -e "\n Importing Census Data"
+LOG "## Importing Census Data"
+
 RUN fetch_csv model-inputs \
   exposure/census-ref-sauid/census-attributes-2016.csv?ref=ab1b2d58dcea80a960c079ad2aff337bc22487c5
 RUN run_psql Create_table_2016_census_v3.sql
 
 
-echo -e "\n Importing Sovi"
+LOG "## Importing Sovi"
 # Need to source tables
 RUN fetch_csv model-inputs \
   social-vulnerability/social-vulnerability-census_2021.csv
@@ -407,26 +411,26 @@ RUN run_psql Create_table_sovi_index_canada_v2.sql
 RUN run_psql Create_table_sovi_census_canada.sql
 RUN run_psql Create_table_sovi_thresholds.sql
 
-echo -e "\n Importing LUTs"
+LOG "## Importing LUTs"
 RUN fetch_csv model-inputs \
   exposure/general-building-stock/documentation/collapse_probability.csv?ref=73d15ca7e48291ee98d8a8dd7fb49ae30548f34e
 RUN run_psql Create_collapse_probability_table.sql
 
-# Retrofit Costs
+LOG "## Retrofit Costs"
 RUN fetch_csv model-inputs \
   exposure/general-building-stock/documentation/retrofit_costs.csv?ref=73d15ca7e48291ee98d8a8dd7fb49ae30548f34e
 RUN run_psql Create_retrofit_costs_table.sql
 
-echo -e "\n Importing GHSL"
+LOG "## Importing GHSL"
 RUN fetch_csv model-inputs \
   natural-hazards/mh-intensity-ghsl.csv?ref=ab1b2d58dcea80a960c079ad2aff337bc22487c5
 RUN run_psql Create_table_GHSL.sql
 
-echo -e "\n Importing MH Intensity"
+LOG "## Importing MH Intensity"
 RUN fetch_csv model-inputs \
   natural-hazards/HTi_sauid_2021.csv
 
-echo -e "\n Importing Hazard Threat Thresholds"
+LOG "## Importing Hazard Threat Thresholds"
 RUN fetch_csv model-inputs \
   natural-hazards/HTi_thresholds_2021.csv
 
@@ -435,17 +439,17 @@ RUN run_psql Create_table_mh_thresholds.sql
 RUN run_psql Create_MH_risk_building_ALL.sql
 RUN run_psql Create_MH_risk_sauid_ALL.sql
 
-# Use python to run \copy from a system call
+LOG '## Use python to run \copy from a system call'
 RUN python3 copyAncillaryTables.py
 
 
 
-# Perform update operations on all tables after data copied into tables
+LOG "## Perform update operations on all tables after data copied into tables"
 RUN run_psql Create_all_tables_update.sql
 RUN run_psql Create_site_exposure_to_building_and_sauid.sql
 RUN run_psql Create_table_vs_30_BC_CAN_model_update_site_exposure.sql
 
-echo -e "\n Generate Indicators"
+LOG "## Generate Indicators"
 RUN run_psql Create_physical_exposure_building_indicators_PhysicalExposure.sql
 RUN run_psql Create_physical_exposure_sauid_indicators_view_PhysicalExposure.sql
 RUN run_psql Create_physical_exposure_building_indicators_PhysicalExposure_ste.sql
@@ -460,9 +464,11 @@ RUN run_psql Create_MH_risk_sauid_ALL.sql
 #######################     Process PSRA                             #######################
 ############################################################################################
 
-echo "Importing Raw PSRA Tables"
+LOG "# Process PSRA"
 
-# Get list of provinces & territories
+LOG "## Importing Raw PSRA Tables"
+
+LOG "### Get list of provinces & territories"
 RUN curl -H "Authorization: token ${GITHUB_TOKEN}" \
   --retry 999 --retry-max-time 0 \
   -o output.json \
@@ -472,7 +478,7 @@ RUN curl -H "Authorization: token ${GITHUB_TOKEN}" \
 
 PT_LIST=($(jq -r '.[].name' output.json))
 
-# cDamage
+LOG "### cDamage"
 RUN fetch_psra_csv_from_model cDamage
 
 for PT in ${PT_LIST[@]}; do
@@ -482,7 +488,7 @@ for PT in ${PT_LIST[@]}; do
   )
 done
 
-# cHazard
+LOG "### cHazard"
 RUN fetch_psra_csv_from_model cHazard
 
 for PT in ${PT_LIST[@]}; do
@@ -491,7 +497,7 @@ for PT in ${PT_LIST[@]}; do
   )
 done
 
-# eDamage
+LOG "### eDamage"
 RUN fetch_psra_csv_from_model eDamage
 
 for PT in ${PT_LIST[@]}; do
@@ -501,7 +507,7 @@ for PT in ${PT_LIST[@]}; do
   )
 done
 
-# ebRisk
+LOG "### ebRisk"
 RUN fetch_psra_csv_from_model ebRisk
 
 for PT in ${PT_LIST[@]}; do
@@ -518,10 +524,10 @@ for PT in ${PT_LIST[@]}; do
   )
 done
 
-# PSRA_0
+LOG "## PSRA_0"
 RUN run_psql psra_0.create_psra_schema.sql
 
-# PSRA_1-8
+LOG "## PSRA_1-8"
 for PT in ${PT_LIST[@]}
 do
   RUN python3 PSRA_runCreate_tables.py --province=${PT} --sqlScript="psra_1.Create_tables.sql"
@@ -538,9 +544,9 @@ RUN run_psql psra_6.Create_psra_merge_into_national_indicators.sql
 #######################     Process DSRA                             #######################
 ############################################################################################
 
-echo "Process DSRA"
+LOG "# Process DSRA"
 
-# Get list of earthquake scenarios
+LOG "## Get list of earthquake scenarios"
 RUN curl -H "Authorization: token ${GITHUB_TOKEN}" \
   --retry 999 --retry-max-time 0 \
   -o FINISHED.json \
@@ -552,13 +558,13 @@ EQSCENARIO_LIST=($(jq -r '.[].name | scan("(?<=s_lossesbyasset_).*(?=_r2)")' FIN
 # s_lossesbyasset_ACM6p5_Beaufort_r2_299_b.csv → ACM6p5_Beaufort_r2_299_b.csv
 EQSCENARIO_LIST_LONGFORM=($(jq -r '.[].name | scan("(?<=s_lossesbyasset_).*r2.*\\.csv")' FINISHED.json))
 
-echo -e "\n Importing scenario outputs into PostGIS..."
+LOG "## Importing scenario outputs into PostGIS"
 for eqscenario in ${EQSCENARIO_LIST[*]}
 do
   RUN python3 DSRA_outputs2postgres_lfs.py --dsraModelDir=$DSRA_REPOSITORY --columnsINI=DSRA_outputs2postgres.ini --eqScenario=$eqscenario
 done
 
-echo "Importing Shakemap"
+LOG "## Importing Shakemap"
 # Make a list of Shakemaps in the repo and download the raw csv files
 DOWNLOAD_URL_LIST=($(jq -r '.[].url | scan(".*s_shakemap_.*\\.csv")' FINISHED.json))
 for shakemap in ${DOWNLOAD_URL_LIST[*]}
@@ -570,10 +576,11 @@ do
       -o $shakemap_filename \
       -L $shakemap
     DOWNLOAD_URL=$(jq -r '.download_url' ${shakemap_filename})
-      echo $DOWNLOAD_URL
+    LOG $DOWNLOAD_URL
     RUN curl -o $shakemap_filename \
       --retry 999 --retry-max-time 0 \
       -L $DOWNLOAD_URL
+
     # Run Create_table_shakemap.sql
     python3 DSRA_runCreateTableShakemap.py --shakemapFile=$shakemap_filename
 done
@@ -601,10 +608,10 @@ do
     echo " "
 done
 
-echo -e "\n Importing Rupture Model"
+LOG "## Importing Rupture Model"
 python3 DSRA_ruptures2postgres.py --dsraRuptureDir="https://github.com/OpenDRR/scenario-catalogue/tree/master/deterministic/ruptures"
 
-echo -e "\n Generating indicator views..."
+LOG "## Generating indicator views"
 for item in ${EQSCENARIO_LIST_LONGFORM[*]}
 do
     SITE=$(echo $item | cut -f5- -d_ | cut -c 1-1)
@@ -625,27 +632,29 @@ do
     fi
 done
 
-echo -e "\n Create Scenario Risk Master Tables at multiple aggregations"
+LOG "## Create Scenario Risk Master Tables at multiple aggregations"
 RUN run_psql Create_scenario_risk_master_tables.sql
 
 ############################################################################################
-#######################     Import Data from PostGIS to ElasticSearch   ####################
+#######################     Import Data from PostGIS to Elasticsearch   ####################
 ############################################################################################
+
+LOG "# Import Data from PostGIS to Elasticsearch"
 
 if [[ ! -z "$ES_USER" ]]; then
   ES_CREDENTIALS="--user ${ES_USER}:${ES_PASS}"
 fi
 
-# Make sure Elasticsearch is ready prior to creating indexes
+LOG "## Make sure Elasticsearch is ready prior to creating indexes"
 until $(curl -sSf -XGET --insecure ${ES_CREDENTIALS:-""} "${ES_ENDPOINT}/_cluster/health?wait_for_status=yellow" > /dev/null); do
-    printf 'No status yellow from Elasticsearch, trying again in 10 seconds \n'
+    LOG "No status yellow from Elasticsearch, trying again in 10 seconds"
     sleep 10
 done
 
-# Load Probabilistic Model Indicators
+LOG "## Load Probabilistic Model Indicators"
 if [ "$loadPsraModels" = true ]
 then
-    echo "Creating PSRA indices in ElasticSearch"
+    LOG "Creating PSRA indices in Elasticsearch"
     for PT in ${PT_LIST[*]}
     do
       RUN python3 psra_postgres2es.py --province=$PT --dbview="all_indicators" --idField="building"
@@ -655,7 +664,7 @@ then
       RUN python3 srcLoss_postgres2es.py --province=$PT
     done
 
-    echo "Creating PSRA Kibana Index Patterns"
+    LOG "Creating PSRA Kibana Index Patterns"
     RUN curl -X POST -H "securitytenant: global" -H "Content-Type: application/json" "${KIBANA_ENDPOINT}/api/saved_objects/index-pattern/psra*all_indicators_s" -H "kbn-xsrf: true" -d '{ "attributes": { "title":"psra*all_indicators_s"}}'
     RUN curl -X POST -H "securitytenant: global" -H "Content-Type: application/json" "${KIBANA_ENDPOINT}/api/saved_objects/index-pattern/psra*all_indicators_b" -H "kbn-xsrf: true" -d '{ "attributes": { "title":"psra*all_indicators_b"}}'
     RUN curl -X POST -H "securitytenant: global" -H "Content-Type: application/json" "${KIBANA_ENDPOINT}/api/saved_objects/index-pattern/psra_*_hmaps" -H "kbn-xsrf: true" -d '{ "attributes": { "title":"psra_*_hmaps"}}'
@@ -668,7 +677,7 @@ if [ "$loadDsraScenario" = true ]
 then
     for eqscenario in ${EQSCENARIO_LIST[*]}
     do
-        echo -e "\nCreating elasticsearch indexes for DSRA..."
+        LOG "Creating Elasticsearch indexes for DSRA"
         RUN python3 dsra_postgres2es.py --eqScenario=$eqscenario --dbview="all_indicators" --idField="building"
         RUN python3 dsra_postgres2es.py --eqScenario=$eqscenario --dbview="all_indicators" --idField="sauid"
     done
@@ -703,7 +712,7 @@ then
 fi
 
 
-echo -e "\n Loading Kibana Saved Objects"
+LOG "# Loading Kibana Saved Objects"
 RUN curl -X POST -H "securitytenant: global" "${KIBANA_ENDPOINT}/api/saved_objects/_import" -H "kbn-xsrf: true" --form file=@kibanaSavedObjects.ndjson
 
 
