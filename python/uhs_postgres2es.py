8018d69c00268b84a2dedcc3c013cf54d7fb6d28
drotheram <drew.rotheram-clarke@canada.ca>
2021-06-07 17:07:05 -0700

    simplify uhs loading

--- python/uhs_postgres2es.py
+++ python/uhs_postgres2es.py
@@ -8,129 +8,41 @@
 #               Joost van Ulden <joost.vanulden@canada.ca>
 # =================================================================
 
-import json
-import os
-import sys
-import psycopg2
-import configparser
-import logging
+import utils 
 import argparse
-import decimal
 
-from elasticsearch import Elasticsearch
-from elasticsearch import helpers
-
-'''
-Script to convert uhs views to ElasticSearch Index
-Can be run from the command line with mandatory arguments
-Run this script with a command like:
-python3 uhs_postgres2es.py --province=${PT}
-'''
-
-
-# Main Function
 def main():
-    logFileName = '{}.log'.format(os.path.splitext(sys.argv[0])[0])
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(levelname)s - %(message)s',
-                        handlers=[logging.FileHandler(logFileName),
-                                  logging.StreamHandler()])
-    auth = get_config_params('config.ini')
     args = parse_args()
-    view = "psra_{province}_uhs".format(**{
-        'province': args.province.lower()})
-    limit = 10000
-    offset = 0
-
-    # create index
-    es = Elasticsearch([auth.get('es', 'es_endpoint')],
-                       http_auth=(auth.get('es', 'es_un'),
-                       auth.get('es', 'es_pw')))
-    if es.indices.exists(view):
-        es.indices.delete(view)
-
-    # id_field = 'AssetID'
-    settings = {
-        'settings': {
-            'number_of_shards': 1,
-            'number_of_replicas': 0
-        },
-        'mappings': {
-            'properties': {
-                'coordinates': {
-                    'type': 'geo_point'
-                },
-                'geometry': {
-                    'type': 'geo_shape'
+    table = utils.PostGISdataset(
+        utils.PostGISConnection(),
+        utils.ESConnection(settings = {
+            'settings': {
+                'number_of_shards': 1,
+                'number_of_replicas': 0
+            },
+            'mappings': {
+                'properties': {
+                    'coordinates': {
+                        'type': 'geo_point'
+                    },
+                    'geometry': {
+                        'type': 'geo_shape'
+                    }
                 }
             }
-        }
-    }
-    es.indices.create(index=view, body=settings, request_timeout=90)
-
-    while True:
+        } ), 
+        view="psra_{province}_uhs".format(**{
+            'province': args.province.lower()}),
         sqlquerystring = 'SELECT *, ST_AsGeoJSON(geom) \
-            FROM results_psra_{province}.{view} \
-            ORDER BY {view}."geom" \
-            LIMIT {limit} \
-            OFFSET {offset}'.format(**{'province': args.province.lower(),
-                                       'view': view,
-                                       'limit': limit,
-                                       'offset': offset})
-        offset += limit
-        connection = None
-        try:
-            # Connect to the PostGIS database
-            connection = psycopg2.connect(user=auth.get('rds',
-                                                        'postgres_un'),
-                                          password=auth.get('rds',
-                                                            'postgres_pw'),
-                                          host=auth.get('rds',
-                                                        'postgres_host'),
-                                          port=auth.get('rds',
-                                                        'postgres_port'),
-                                          database=auth.get('rds',
-                                                            'postgres_db'))
-            # Query the entire view with the geometries in geojson format
-            cur = connection.cursor()
-            cur.execute(sqlquerystring)
-            rows = cur.fetchall()
-            if rows:
-                columns = [name[0] for name in cur.description]
-                geomIndex = columns.index('st_asgeojson')
-                feature_collection = {'type': 'FeatureCollection',
-                                      'features': []}
-                # Format table into a geojson format for ES/Kibana consumption
-                for row in rows:
-                    coordinates = json.loads(row[geomIndex])['coordinates']
-                    feature = {
-                        'type': 'Feature',
-                        'geometry': json.loads(row[geomIndex]),
-                        'coordinates': coordinates,
-                        'properties': {},
-                    }
-                    for index, column in enumerate(columns):
-                        if column != "st_asgeojson":
-                            value = row[index]
-                            feature['properties'][column] = value
+                FROM results_psra_{province}.psra_{province}_uhs \
+                ORDER BY psra_{province}_uhs."geom" \
+                LIMIT {{limit}} \
+                OFFSET {{offset}}'.format(**{'province': args.province.lower()})
+    )
 
-                    feature_collection['features'].append(feature)
-                geojsonobject = json.dumps(feature_collection,
-                                           indent=2,
-                                           default=decimal_default)
-                d = json.loads(geojsonobject)
-                helpers.bulk(es,
-                             gendata(d, view),
-                             raise_on_error=False)
+    table.postgis2es()
 
-            else:
-                if(connection):
-                    connection.close()
-                return
-
-        except (Exception, psycopg2.Error) as error:
-            logging.error(error)
-
+    return
 
 def parse_args():
     parser = argparse.ArgumentParser(description="script description")
