266ecad858cf6d8696109ef6f9d20da77931d332
drotheram <drew.rotheram-clarke@canada.ca>
2021-06-15 10:23:03 -0700

    change repo src: model-inputs to openquake-inputs

--- python/add_data.sh
+++ python/add_data.sh
@@ -385,7 +385,7 @@ read_github_token() {
 # from the OpenDRR/model-factory repository
 get_model_factory_scripts() {
   # TODO: Make this more robust
-  RUN git clone https://github.com/OpenDRR/model-factory.git --depth 1 || (cd model-factory ; RUN git pull)
+  RUN git clone https://github.com/OpenDRR/model-factory.git --branch update_source_to_openquake-inputs --depth 1 || (cd model-factory ; RUN git pull)
 
   # Copy model-factory scripts to working directory
   # TODO: Find ways to keep these scripts in their place without copying them all to WORKDIR
@@ -400,7 +400,7 @@ get_git_lfs_pointers_of_csv_files() {
   LOG '## Fetch Git LFS pointers of CSV files for "oid sha256"'
   mkdir -p git
   ( cd git &&
-    for repo in canada-srm2 model-inputs scenario-catalogue; do
+    for repo in canada-srm2 model-inputs openquake-inputs scenario-catalogue; do
       RUN git clone --filter=blob:none --no-checkout "https://${GITHUB_TOKEN}@github.com/OpenDRR/${repo}.git"
       is_dry_run || \
         ( cd $repo && \
@@ -459,8 +459,8 @@ import_physical_exposure_model() {
   # Physical Exposure
   LOG "## Importing Physical Exposure Model into PostGIS"
 
-  RUN fetch_csv model-inputs \
-    exposure/general-building-stock/BldgExpRef_CA_master_v3p1.csv
+  RUN fetch_csv openquake-inputs \
+    exposure/general-building-stock/BldgExpRef_CA_master_v3p2.csv
   RUN run_psql Create_table_canada_exposure.sql
 
   RUN fetch_csv model-inputs \
@@ -472,10 +472,10 @@ import_vs30_model() {
   # VS30
   LOG "## Importing VS30 Model into PostGIS"
 
-  RUN fetch_csv model-inputs \
+  RUN fetch_csv openquake-inputs \
     earthquake/sites/regions/vs30_CAN_site_model_xref.csv
 
-  RUN fetch_csv model-inputs \
+  RUN fetch_csv openquake-inputs \
     earthquake/sites/regions/site-vgrid_CA.csv
 
   RUN run_psql Create_table_vs_30_CAN_site_model.sql
@@ -486,19 +486,19 @@ import_census_data() {
   # Census Data
   LOG "## Importing Census Data"
 
-  RUN fetch_csv model-inputs \
-    exposure/census-ref-sauid/census-attributes-2016.csv?ref=ab1b2d58dcea80a960c079ad2aff337bc22487c5
+  RUN fetch_csv openquake-inputs \
+    exposure/census-ref-sauid/census-attributes-2016.csv
   RUN run_psql Create_table_2016_census_v3.sql
 }
 
 import_sovi() {
   LOG "## Importing Sovi"
   # Need to source tables
-  RUN fetch_csv model-inputs \
+  RUN fetch_csv openquake-inputs \
     social-vulnerability/social-vulnerability-census_2021.csv
-  RUN fetch_csv model-inputs \
+  RUN fetch_csv openquake-inputs \
     social-vulnerability/social-vulnerability-index_2021.csv
-  RUN fetch_csv model-inputs \
+  RUN fetch_csv openquake-inputs \
     social-vulnerability/sovi_thresholds_2021.csv
 
   RUN run_psql Create_table_sovi_index_canada_v2.sql
@@ -508,35 +508,35 @@ import_sovi() {
 
 import_luts() {
   LOG "## Importing LUTs"
-  RUN fetch_csv model-inputs \
-    exposure/general-building-stock/documentation/collapse_probability.csv?ref=73d15ca7e48291ee98d8a8dd7fb49ae30548f34e
+  RUN fetch_csv openquake-inputs \
+    exposure/general-building-stock/1. documentation/collapse_probability.csv
   RUN run_psql Create_collapse_probability_table.sql
 }
 
-import_retrofit_costs() {
-  LOG "## Retrofit Costs"
-  RUN fetch_csv model-inputs \
-    exposure/general-building-stock/documentation/retrofit_costs.csv?ref=73d15ca7e48291ee98d8a8dd7fb49ae30548f34e
-  RUN run_psql Create_retrofit_costs_table.sql
-}
+# import_retrofit_costs() {
+#   LOG "## Retrofit Costs"
+#   RUN fetch_csv openquake-inputs \
+#     exposure/general-building-stock/1. documentation/retrofit_costs.csv
+#   RUN run_psql Create_retrofit_costs_table.sql
+# }
 
 import_ghsl() {
   LOG "## Importing GHSL"
-  RUN fetch_csv model-inputs \
-    natural-hazards/mh-intensity-ghsl.csv?ref=ab1b2d58dcea80a960c079ad2aff337bc22487c5
+  RUN fetch_csv openquake-inputs \
+    natural-hazards/mh-intensity-ghsl.csv
   RUN run_psql Create_table_GHSL.sql
 }
 
 import_mh_intensity() {
   LOG "## Importing MH Intensity"
-  RUN fetch_csv model-inputs \
-    natural-hazards/HTi_sauid_2021.csv
+  RUN fetch_csv openquake-inputs \
+    natural-hazards/HTi_sauid.csv
 }
 
 import_hazard_threat_thresholds() {
   LOG "## Importing Hazard Threat Thresholds"
-  RUN fetch_csv model-inputs \
-    natural-hazards/HTi_thresholds_2021.csv
+  RUN fetch_csv openquake-inputs \
+    natural-hazards/HTi_thresholds.csv
 }
 
 post_process_mh_tables() {
