3e9e6492d4589afa3a168bdd22ce2e031fe1bf2a
drotheram <drew.rotheram-clarke@canada.ca>
2022-02-04 16:46:19 -0800

    remove global fabric null value bug

--- python/exposure_postgres2es.py
+++ python/exposure_postgres2es.py
@@ -214,30 +214,30 @@ def main():
     table.postgis2es()
 
     # hexbin global fabric
-    table = utils.PostGISdataset(
-        utils.PostGISConnection(),
-        utils.ESConnection(settings={
-            'settings': {
-                'number_of_shards': 1,
-                'number_of_replicas': 0
-            },
-            'mappings': {
-                'properties': {
-                    'geometry': {
-                        'type': 'geo_shape'
-                    }
-                }
-            }
-        }),
-        view="opendrr_nhsl_physical_exposure_indicators_hexbin_global_fabric",
-        sqlquerystring='SELECT *, ST_AsGeoJSON(geom) \
-            FROM \
-            results_nhsl_physical_exposure.nhsl_physical_exposure_indicators_hexbin_global_fabric \
-            ORDER BY nhsl_physical_exposure_indicators_hexbin_global_fabric."gridid" \
-            LIMIT {limit} \
-            OFFSET {offset}'
-    )
-    table.postgis2es()
+    # table = utils.PostGISdataset(
+    #     utils.PostGISConnection(),
+    #     utils.ESConnection(settings={
+    #         'settings': {
+    #             'number_of_shards': 1,
+    #             'number_of_replicas': 0
+    #         },
+    #         'mappings': {
+    #             'properties': {
+    #                 'geometry': {
+    #                     'type': 'geo_shape'
+    #                 }
+    #             }
+    #         }
+    #     }),
+    #     view="opendrr_nhsl_physical_exposure_indicators_hexbin_global_fabric",
+    #     sqlquerystring='SELECT *, ST_AsGeoJSON(geom) \
+    #         FROM \
+    #         results_nhsl_physical_exposure.nhsl_physical_exposure_indicators_hexbin_global_fabric \
+    #         ORDER BY nhsl_physical_exposure_indicators_hexbin_global_fabric."gridid" \
+    #         LIMIT {limit} \
+    #         OFFSET {offset}'
+    # )
+    # table.postgis2es()
 
     return
 
