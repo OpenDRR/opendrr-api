e50c048ef1edffaf4af5fcb7bfd67994efad20d2
Joost van Ulden <joost.vanulden@canada.ca>
2022-05-05 15:37:18 -0700

    Update gen_pygeoapi_config.py

--- python/gen_pygeoapi_config.py
+++ python/gen_pygeoapi_config.py
@@ -9,45 +9,45 @@ from elasticsearch import Elasticsearch
 # Main Function
 def main():
 
-    auth = get_config_params('config.ini')
+    auth = get_config_params("config.ini")
 
     # es = Elasticsearch()
-    # es = Elasticsearch([auth.get('es', 'es_endpoint')],
-    #                    http_auth=(auth.get('es', 'es_un'),
-    #                               auth.get('es', 'es_pw')))
+    # es = Elasticsearch([auth.get("es", "es_endpoint")],
+    #                    http_auth=(auth.get("es", "es_un"),
+    #                               auth.get("es", "es_pw")))
 
-    es = Elasticsearch([auth.get('es', 'es_endpoint')])
+    es = Elasticsearch([auth.get("es", "es_endpoint")])
 
     text_file = open("../pygeoapi/opendrr.config.yml", "w")
 
-    with open('../opendrr_config_template.yml', 'r') as f:
+    with open("../opendrr_config_template.yml", "r") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
 
-    lyrs = config['resources']
+    lyrs = config["resources"]
 
-    indices = es.indices.get('*')
+    indices = es.indices.get("*")
 
     for k in list(lyrs.keys()):
         if k not in indices:
-            print('REMOVING TEMPLATE ENTRY FOR: ' + k)
+            print("REMOVING TEMPLATE ENTRY FOR: " + k)
             del lyrs[k]
         else:
             # write in the ES endpoint configured in the config.ini
-            new = lyrs[k]['providers'][0]['data'].replace(
-                'ES_ENDPOINT',
+            new = lyrs[k]["providers"][0]["data"].replace(
+                "ES_ENDPOINT",
                 auth.get(
-                    'es',
-                    'es_endpoint'
+                    "es",
+                    "es_endpoint"
                     )
                 )
-            lyrs[k]['providers'][0]['data'] = new
+            lyrs[k]["providers"][0]["data"] = new
 
     # list indices missing from opendrr_config_template.yml
     for i in list(indices.keys()):
         if i not in lyrs:
-            print('MISSING IN CONFIGURATION TEMPLATE: ' + i)
+            print("MISSING IN CONFIGURATION TEMPLATE: " + i)
 
-    config['resources'] = lyrs
+    config["resources"] = lyrs
 
     text_file.write(yaml.dump(config))
     text_file.close()
@@ -62,5 +62,5 @@ def get_config_params(args):
     return configParseObj
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
