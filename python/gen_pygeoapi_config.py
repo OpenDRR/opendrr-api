1134f60bb50ec77f8b209b1a82d26071508e7d49
Joost van Ulden <joost.vanulden@canada.ca>
2022-06-13 11:29:27 -0700

    Versioning collections
    Added parameter to config to select version desired. Removed version from collection name.

--- python/gen_pygeoapi_config.py
+++ python/gen_pygeoapi_config.py
@@ -23,30 +23,49 @@ def main():
     with open("../opendrr_config_template.yml", "r") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
 
+    version = auth.get("es", "index_version")
+
     lyrs = config["resources"]
 
-    indices = es.indices.get("*")
+    indices = es.indices.get(index="opendrr_*")
 
+    print("\nProcessing opendrr_config_template.yml...")
     for k in list(lyrs.keys()):
-        if k not in indices:
+        l = k + '_' + version
+        if l not in indices:
             print("REMOVING TEMPLATE ENTRY FOR: " + k)
             del lyrs[k]
         else:
             # write in the ES endpoint configured in the config.ini
             new = lyrs[k]["providers"][0]["data"].replace(
-                "ES_ENDPOINT", auth.get("es", "es_endpoint")
-            )
+                    "ES_ENDPOINT", auth.get("es", "es_endpoint")
+                )
+            new = new.replace(
+                    "INDEX_VERSION", version
+                )
+            
             lyrs[k]["providers"][0]["data"] = new
 
-    # list indices missing from opendrr_config_template.yml
-    for i in list(indices.keys()):
-        if i not in lyrs:
-            print("MISSING IN CONFIGURATION TEMPLATE: " + i)
+    print("\nDone!")
 
+    # list indices missing from opendrr_config_template.yml
+    print("\nListing indices missing from opendrr_config_template.yml...")
+    for i in list(indices.keys()):
+        if version not in i:
+            continue
+        if i.replace('_' + version, '') not in lyrs:
+            print("MISSING IN CONFIGURATION TEMPLATE: " + i)
+    
+    print("\nDone!")
+    
+    # write the layers to the configuration resources element
     config["resources"] = lyrs
 
+    # save the configuration
+    print("\nSaving configuration file to disk...")
     text_file.write(yaml.dump(config))
     text_file.close()
+    print("\nDone!\n")
 
 
 def get_config_params(args):
