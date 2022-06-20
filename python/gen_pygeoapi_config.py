d7e0eeb4c437b55684d9f627b10030e368e8996e
Joost van Ulden <joost.vanulden@canada.ca>
2022-06-20 11:20:44 -0700

    Add opendrr psra canada (#211)
    * Update opendrr_config_template.yml
    
    Added PSRA Canada Agg Loss layers to pygeoapi
    
    * Versioning collections
    
    Added parameter to config to select version desired. Removed version from collection name.
    
    * Updated configuration files
    
    * Fix python linting errors
    
    * Fix linting errors
    
    * Fix linting errors
    
    * Update opendrr.openapi.yml
    
    * Update opendrr.openapi.yml
    
    * Update requirements.txt

--- python/gen_pygeoapi_config.py
+++ python/gen_pygeoapi_config.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 
+"""
+Generate configuration files for OpenDRR pygeoapi (OGC API - Features)
+"""
+
 import configparser
 
 import yaml
 from elasticsearch import Elasticsearch
 
 
-# Main Function
 def main():
+    """
+    Main function
+    """
 
     auth = get_config_params("config.ini")
 
@@ -16,46 +22,63 @@ def main():
     #                    http_auth=(auth.get("es", "es_un"),
     #                               auth.get("es", "es_pw")))
 
-    es = Elasticsearch([auth.get("es", "es_endpoint")])
+    elasticsearch = Elasticsearch([auth.get("es", "es_endpoint")])
 
-    text_file = open("../pygeoapi/opendrr.config.yml", "w")
+    text_file = open("../pygeoapi/opendrr.config.yml", "w", encoding="utf-8")
 
-    with open("../opendrr_config_template.yml", "r") as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
+    with open("../opendrr_config_template.yml", "r", encoding="utf-8") as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+
+    version = auth.get("es", "index_version")
 
     lyrs = config["resources"]
 
-    indices = es.indices.get("*")
+    indices = elasticsearch.indices.get(index="opendrr_*")
 
+    print("\nProcessing opendrr_config_template.yml...")
     for k in list(lyrs.keys()):
-        if k not in indices:
-            print("REMOVING TEMPLATE ENTRY FOR: " + k)
-            del lyrs[k]
+        k_version = k + "_" + version
+        if k_version not in indices:
+            print("REMOVING TEMPLATE ENTRY FOR: " + k_version)
+            del lyrs[k_version]
         else:
             # write in the ES endpoint configured in the config.ini
             new = lyrs[k]["providers"][0]["data"].replace(
                 "ES_ENDPOINT", auth.get("es", "es_endpoint")
             )
+            new = new.replace("INDEX_VERSION", version)
+
             lyrs[k]["providers"][0]["data"] = new
 
-    # list indices missing from opendrr_config_template.yml
-    for i in list(indices.keys()):
-        if i not in lyrs:
-            print("MISSING IN CONFIGURATION TEMPLATE: " + i)
+    print("\nDone!")
 
+    # list indices missing from opendrr_config_template.yml
+    print("\nListing indices missing from opendrr_config_template.yml...")
+    for i in list(indices.keys()):
+        if version not in i:
+            continue
+        if i.replace("_" + version, "") not in lyrs:
+            print("MISSING IN CONFIGURATION TEMPLATE: " + i)
+    print("\nDone!")
+
+    # write the layers to the configuration resources element
     config["resources"] = lyrs
 
+    # save the configuration
+    print("\nSaving configuration file to disk...")
     text_file.write(yaml.dump(config))
     text_file.close()
+    print("\nDone!\n")
 
 
 def get_config_params(args):
     """
     Parse Input/Output columns from supplied *.ini file
     """
-    configParseObj = configparser.ConfigParser()
-    configParseObj.read(args)
-    return configParseObj
+
+    config_parse_obj = configparser.ConfigParser()
+    config_parse_obj.read(args)
+    return config_parse_obj
 
 
 if __name__ == "__main__":
