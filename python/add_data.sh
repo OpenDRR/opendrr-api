OpenDRR/opendrr-api

commit 5813fe61ebd39965444b454c2e2ece1cc85a6a02

Change test private repository in read_github_token()
Switch from OpenDRR/seismic-risk-model to OpenDRR/DSRA-processing
for GitHub token validity test because the former has been made public
while the latter is still private.

python/add_data.sh
@@ -437,11 +437,15 @@ read_github_token() {
     WARN "Your GITHUB_TOKEN has a length of ${#GITHUB_TOKEN} characters, but 40 or above is expected."
   fi
 
+  # TEST_PRIVATE_REPO is set to any private Git repository within the
+  # OpenDRR organization, for testing the validity of the user's GitHub token
+  TEST_PRIVATE_REPO=OpenDRR/DSRA-processing
+
   tmpfile=$(mktemp)
   status_code=$(curl --write-out "%{http_code}" --silent --output "$tmpfile" \
     -H "Authorization: token ${GITHUB_TOKEN}" \
-    -L https://api.github.com/repos/OpenDRR/seismic-risk-model/contents/eDamage/output)
-  INFO "Access to OpenDRR/seismic-risk-model returns HTTP status code $status_code"
+    -L https://api.github.com/repos/${TEST_PRIVATE_REPO}/contents/eDamage/output)
+  INFO "Access to test private repo ${TEST_PRIVATE_REPO} returns HTTP status code $status_code"
 
   if [[ "$status_code" -ne 200 ]] ; then
     cat "$tmpfile"
@@ -450,7 +454,7 @@ read_github_token() {
         ERROR "Your GitHub token is invalid or has expired. Aborting..."
         ;;
       404)
-        ERROR "Your GitHub token was unable to access https://github.com/OpenDRR/seismic-risk-model. Please ensure the \"repo\" scope is enabled for the token. Aborting..."
+        ERROR "Your GitHub token was unable to access ${TEST_PRIVATE_REPO}. Please ensure the \"repo\" scope is enabled for the token. Aborting..."
         ;;
       *)
         ERROR "Unhandled error ($status_code): Please try again. Aborting..."

commit 063160abed32060161233d627cdc68baf5da50b0

Fix erroneous 404 error in read_github_token()
Fixes a bug that I carelessly introduced
in commit 5813fe61ebd39965444b454c2e2ece1cc85a6a02

Thanks to William Chow for catching the bug!

python/add_data.sh
@@ -442,11 +442,12 @@ read_github_token() {
   # TEST_PRIVATE_REPO is set to any private Git repository within the
   # OpenDRR organization, for testing the validity of the user's GitHub token
   TEST_PRIVATE_REPO=OpenDRR/DSRA-processing
+  TEST_PRIVATE_FILE=README.md
 
   tmpfile=$(mktemp)
   status_code=$(curl --write-out "%{http_code}" --silent --output "$tmpfile" \
     -H "Authorization: token ${GITHUB_TOKEN}" \
-    -L https://api.github.com/repos/${TEST_PRIVATE_REPO}/contents/eDamage/output)
+    -L https://api.github.com/repos/${TEST_PRIVATE_REPO}/contents/${TEST_PRIVATE_FILE})
   INFO "Access to test private repo ${TEST_PRIVATE_REPO} returns HTTP status code $status_code"
 
   if [[ "$status_code" -ne 200 ]] ; then