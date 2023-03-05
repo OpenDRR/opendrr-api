OpenDRR/opendrr-api

commit 399aa4ab63769d47dafb20305de6ddad06708bd8

Simplify get_git_lfs_pointers_of_csv_files() by including owner in $repo

python/add_data.sh
@@ -487,18 +487,13 @@ get_git_lfs_pointers_of_csv_files() {
   rm -rf "$base_dir"
   mkdir -p "$base_dir"
   ( cd "$base_dir" && \
-    for repo in seismic-risk-model model-inputs openquake-inputs; do
-      RUN git clone --filter=blob:none --no-checkout "https://${GITHUB_TOKEN}@github.com/OpenDRR/${repo}.git"
+    for repo in ${DSRA_REPOSITORY} OpenDRR/openquake-inputs OpenDRR/seismic-risk-model; do
+      RUN git clone --filter=blob:none --no-checkout "https://${GITHUB_TOKEN}@github.com/${repo}.git"
       is_dry_run || \
-        ( cd $repo && \
+        ( cd "$repo" && \
           git sparse-checkout set '*.csv' && \
           GIT_LFS_SKIP_SMUDGE=1 git checkout )
     done
-    RUN git clone --filter=blob:none --no-checkout "https://${GITHUB_TOKEN}@github.com/${DSRA_REPOSITORY}.git"
-      is_dry_run || \
-        ( cd $repo && \
-          git sparse-checkout set '*.csv' && \
-          GIT_LFS_SKIP_SMUDGE=1 git checkout )
   )
 }
 

python/build_exposure_ancillary.sh
@@ -433,8 +433,8 @@ get_git_lfs_pointers_of_csv_files() {
   rm -rf "$base_dir"
   mkdir -p "$base_dir"
   ( cd "$base_dir" && \
-    for repo in seismic-risk-model model-inputs openquake-inputs earthquake-scenarios; do
-      RUN git clone --filter=blob:none --no-checkout "https://${GITHUB_TOKEN}@github.com/OpenDRR/${repo}.git"
+    for repo in OpenDRR/earthquake-scenarios OpenDRR/openquake-inputs OpenDRR/seismic-risk-model; do
+      RUN git clone --filter=blob:none --no-checkout "https://${GITHUB_TOKEN}@github.com/${repo}.git"
       is_dry_run || \
         ( cd $repo && \
           git sparse-checkout set '*.csv' && \

commit 0d6d3ae76d945faf66aa718d402881751ed68a3f

Fix error in "cd" command in get_git_lfs_pointers_of_csv_files()
I earlier changed the repo to include the owner, i.e. by changing
e.g. earthquake-scenarios to OpenDRR/earthquake-scenarios,
but forgot to do a basename so as to "cd" to the correct directory path,
causing the stack build run to fail prematurely.

Fixes a bug that I again carelessly introduced in
commit 399aa4ab63769d47dafb20305de6ddad06708bd8

python/add_data.sh
@@ -3,14 +3,14 @@
 #
 # add_data.sh - Populate PostGIS database for Elasticsearch
 #
-# Copyright (C) 2020-2022 Government of Canada
+# Copyright (C) 2020-2023 Government of Canada
 #
-# Main Authors: Drew Rotheram-Clarke <drew.rotheram-clarke@canada.ca>
-#               Joost van Ulden <joost.vanulden@canada.ca>
-#               Anthony Fok <anthony.fok@nrcan-rncan.gc.ca>
+# Authors: Drew Rotheram-Clarke <drew.rotheram-clarke@canada.ca>
+#          Joost van Ulden <joost.vanulden@canada.ca>
+#          Anthony Fok <anthony.fok@canada.ca>
 
 trap : TERM INT
-set -e
+set -e -u
 
 ############################################################################################
 ############    Define global variables                                         ############
@@ -19,12 +19,16 @@ set -e
 # Needed variables (defined in .env for Docker Compose, or in a Amazon ECS task definition)
 ENV_VAR_LIST=(
   POSTGRES_USER POSTGRES_PASS POSTGRES_PORT POSTGRES_HOST DB_NAME
-  POPULATE_DB \
-  KIBANA_ENDPOINT ES_ENDPOINT ES_USER ES_PASS \
+  POPULATE_DB
+  KIBANA_ENDPOINT ES_ENDPOINT ES_USER ES_PASS
   loadDsraScenario loadPsraModels loadHazardThreat loadPhysicalExposure
   loadRiskDynamics loadSocialFabric
+  processDSRA processPSRA
+  # autoParseDSRA
+  # dsraScenarioList
 )
 
+ADD_DATA_DRY_RUN=${ADD_DATA_PRINT_FUNCNAME:-false}
 ADD_DATA_PRINT_FUNCNAME=${ADD_DATA_PRINT_FUNCNAME:-true}
 ADD_DATA_PRINT_LINENO=${ADD_DATA_PRINT_LINENO:-true}
 ADD_DATA_REDUCE_DISK_USAGE=${ADD_DATA_REDUCE_DISK_USAGE:-true}
@@ -75,7 +79,7 @@ LOG() {
 
   for i in "$@"; do
     # Hide secrets
-    i="${i//$GITHUB_TOKEN/***}"
+    [[ -v GITHUB_TOKEN ]] && i="${i//$GITHUB_TOKEN/***}"
     [[ ${POSTGRES_PASS,,} != password ]] && i="${i//$POSTGRES_PASS/***}"
     [[ ${ES_PASS,,} != password ]] && i="${i//$ES_PASS/***}"
     # Try to add quotes as appropriate
@@ -412,11 +416,13 @@ setup_eatmydata() {
 
 # check_environment_variables ensures required environment variables are defined,
 # either in Docker Compose .env file, or in Amazon ECS task definition.
+
 check_environment_variables() {
   LOG "## Check needed environment variables"
   for var in "${ENV_VAR_LIST[@]}"; do
-    INFO "$var = ${!var}"
-    if [[ -z ${!var} ]]; then
+    if [[ -v $var ]] && [[ -n ${!var} ]]; then
+      INFO "$var = ${!var}"
+    else
       if [[ "$var" =~ ^(ES_USER|ES_PASS)$ ]]; then
         WARN "Optional variable $var is not set.  Continuing..."
       else
@@ -493,7 +499,7 @@ get_git_lfs_pointers_of_csv_files() {
     for repo in ${DSRA_REPOSITORY} OpenDRR/openquake-inputs OpenDRR/seismic-risk-model; do
       RUN git clone --filter=blob:none --no-checkout "https://${GITHUB_TOKEN}@github.com/${repo}.git"
       is_dry_run || \
-        ( cd "$repo" && \
+        ( cd "$(basename "$repo")" && \
           git sparse-checkout set '*.csv' && \
           GIT_LFS_SKIP_SMUDGE=1 git checkout )
     done
@@ -968,7 +974,7 @@ main() {
   RUN import_exposure_ancillary_db
   RUN download_luts
 
-  if [[ $processPSRA = true ]]; then
+  if [[ $processPSRA == true ]]; then
     LOG "# Processing PSRA"
     RUN import_raw_psra_tables
     RUN post_process_psra_tables
@@ -976,7 +982,7 @@ main() {
     LOG "# Omitting PSRA Processing"
   fi
 
-  if [[ $processDSRA = true ]]; then
+  if [[ $processDSRA == true ]]; then
     LOG "# Process DSRA"
     RUN import_earthquake_scenarios
     RUN import_shakemap