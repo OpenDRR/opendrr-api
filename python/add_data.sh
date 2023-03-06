OpenDRR/opendrr-api

commit 399aa4ab63769d47dafb20305de6ddad06708bd8

Simplify get_git_lfs_pointers_of_csv_files() by including owner in $repo

python/add_data.sh
@@ -487,18 +487,13 @@ get_git_lfs_pointers_of_csv_files() {
   rm -rf "$base_dir"
   mkdir -p "$base_dir"
   ( cd "$base_dir" && \
-    for repo in seismic-risk-model model-inputs openquake-inputs; do
-      RUN git clone --filter=blob:none --no-checkout "https://${GITHUB_TOKEN}@github.com/OpenDRR/${repo}.git"
+    for repo in ${DSRA_REPOSITORY} OpenDRR/openquake-inputs OpenDRR/seismic-risk-model; do
+      RUN git clone --filter=blob:none --no-checkout "https://${GITHUB_TOKEN}@github.com/${repo}.git"
       is_dry_run || \
-        ( cd $repo && \
+        ( cd "$repo" && \
           git sparse-checkout set '*.csv' && \
           GIT_LFS_SKIP_SMUDGE=1 git checkout )
     done
-    RUN git clone --filter=blob:none --no-checkout "https://${GITHUB_TOKEN}@github.com/${DSRA_REPOSITORY}.git"
-      is_dry_run || \
-        ( cd $repo && \
-          git sparse-checkout set '*.csv' && \
-          GIT_LFS_SKIP_SMUDGE=1 git checkout )
   )
 }
 

python/build_exposure_ancillary.sh
@@ -433,8 +433,8 @@ get_git_lfs_pointers_of_csv_files() {
   rm -rf "$base_dir"
   mkdir -p "$base_dir"
   ( cd "$base_dir" && \
-    for repo in seismic-risk-model model-inputs openquake-inputs earthquake-scenarios; do
-      RUN git clone --filter=blob:none --no-checkout "https://${GITHUB_TOKEN}@github.com/OpenDRR/${repo}.git"
+    for repo in OpenDRR/earthquake-scenarios OpenDRR/openquake-inputs OpenDRR/seismic-risk-model; do
+      RUN git clone --filter=blob:none --no-checkout "https://${GITHUB_TOKEN}@github.com/${repo}.git"
       is_dry_run || \
         ( cd $repo && \
           git sparse-checkout set '*.csv' && \

commit 9314e284456c1a2d60053f41a38f2f704fa18a83

Fix error in "cd" command in get_git_lfs_pointers_of_csv_files, Take 2
I earlier changed the repo to include the owner, i.e. by changing
e.g. earthquake-scenarios to OpenDRR/earthquake-scenarios,
but failed to use basename to fix the directory path to "cd" into,
causing the stack build run to fail prematurely.

Fixes a bug that I again carelessly introduced in
commit 399aa4ab63769d47dafb20305de6ddad06708bd8

python/add_data.sh
@@ -493,7 +493,7 @@ get_git_lfs_pointers_of_csv_files() {
     for repo in ${DSRA_REPOSITORY} OpenDRR/openquake-inputs OpenDRR/seismic-risk-model; do
       RUN git clone --filter=blob:none --no-checkout "https://${GITHUB_TOKEN}@github.com/${repo}.git"
       is_dry_run || \
-        ( cd "$repo" && \
+        ( cd "$(basename "$repo")" && \
           git sparse-checkout set '*.csv' && \
           GIT_LFS_SKIP_SMUDGE=1 git checkout )
     done